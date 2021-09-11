df1b45aded4441375163d3464459df8b72b1a3ae
joelaghina <Tosinlabi30@gmail.com>
2021-09-11 14:40:42 +0100

    Updates

---
+++ precache-manifest.1c9510d3f7105e5398d38031978724cf.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ed153722b42b7267296117885b0302f",
+    "url": "/Multi-Signature-Wallet/index.html"
+  },
+  {
+    "revision": "b681dc4b11f0b054672a",
+    "url": "/Multi-Signature-Wallet/static/css/2.24ec10a6.chunk.css"
+  },
+  {
+    "revision": "e29ea02ab8f3432e14ef",
+    "url": "/Multi-Signature-Wallet/static/css/main.94c1e3df.chunk.css"
+  },
+  {
+    "revision": "b681dc4b11f0b054672a",
+    "url": "/Multi-Signature-Wallet/static/js/2.de2342d2.chunk.js"
+  },
+  {
+    "revision": "ac3bdea844e6ba8e60a8a09c876f4343",
+    "url": "/Multi-Signature-Wallet/static/js/2.de2342d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e29ea02ab8f3432e14ef",
+    "url": "/Multi-Signature-Wallet/static/js/main.f6b1b937.chunk.js"
+  },
+  {
+    "revision": "4d40bc72b8f8925817fc",
+    "url": "/Multi-Signature-Wallet/static/js/runtime-main.15532357.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/Multi-Signature-Wallet/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/Multi-Signature-Wallet/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/Multi-Signature-Wallet/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/Multi-Signature-Wallet/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/Multi-Signature-Wallet/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/Multi-Signature-Wallet/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/Multi-Signature-Wallet/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/Multi-Signature-Wallet/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/Multi-Signature-Wallet/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/Multi-Signature-Wallet/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/Multi-Signature-Wallet/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/Multi-Signature-Wallet/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/Multi-Signature-Wallet/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/Multi-Signature-Wallet/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/Multi-Signature-Wallet/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/Multi-Signature-Wallet/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
